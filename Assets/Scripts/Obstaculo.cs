119d45302e334d5cf43f6a0941d54bc5030c09ac
Giovanna L <98828736+giiuk@users.noreply.github.com>
2024-06-12 13:41:38 -0300

    AULA 12-06

--- Assets/Scripts/Obstaculo.cs
+++ Assets/Scripts/Obstaculo.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Obstaculo : MonoBehaviour
 {
     [SerializeField]
@@ -9,6 +10,12 @@ public class Obstaculo : MonoBehaviour
 
     [SerializeField]
     private float variacaoY;
+
+    private Vector3 posicaoPassaro;
+
+    private bool pontuei;
+
+    private UIScript scriptDaUI;
     private void Awake()
     {
         this.transform.Translate(Vector3.up * Random.Range(-variacaoY,variacaoY));
@@ -17,15 +24,29 @@ public class Obstaculo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!this.pontuei && this.transform.position.x < posicaoPassaro.x)
+        {
+            Debug.Log("Pontuou");
+            this.pontuei = true;
+            this.scriptDaUI.adicionarPonto();
+        }
         this.transform.Translate(Vector3.left * velocidade * Time.deltaTime);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Start()
+    {
+        this.posicaoPassaro = GameObject.FindObjectOfType<Bird>().transform.position;
+        this.scriptDaUI = GameObject.FindObjectOfType<UIScript>();
+    }
+
+
+  /*  private void OnTriggerEnter2D(Collider2D collision)
     {
         this.Destruir();
     }
     void Destruir()
     {
         GameObject.Destroy(this.gameObject);
-    }
+    }*/
+
 }
